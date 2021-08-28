5017bf5ff55da85db94fd2c8c49f4a95101e3a5b
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-08-28 17:00:09 +0100

    add page _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        {/* <title>3exitus Marketing Digital</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" /> */}
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="3xitus MArketing Digital, uma empresa que prestadora de serviços na área digital."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-export default MyApp
+
+export default App
