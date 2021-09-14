63303091335e58945475af8eea94bb6df6e1ff48
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-14 15:49:13 +0100

    def minHeight in box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import { Box } from "@chakra-ui/react";
+
+
 export default function Home(){
   return (
-    <h1>Home page</h1>
+      <Box minHeight="70vh">
+          <h1>Home</h1>
+      </Box>
   )
 }
