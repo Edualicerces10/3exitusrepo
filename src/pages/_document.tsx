39e378731edeb66ea993bfe7751d76c161f1e253
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-14 15:48:46 +0100

    add meta datas and links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,27 @@
+import { ColorModeScript } from '@chakra-ui/react'
+
 import Document, {
     Html,
     Head,
     Main,
     NextScript,
-  } from 'next/document'
-  
+  } from 'next/document' 
+import ColorTheme from '../styles/ColorTheme'
+
   export default class MyDocument extends Document {
     render() {
       return (
         <Html lang="pt-PT">
-          <Head />
+          <Head>
+            <link rel="shortcut icon" href="/img/3exitus.png" />
+            <meta name="theme-color" content="#06092B" />
+            <meta
+            name="description"
+            content="3exitus Marketing Digital, uma empresa que prestadora de serviços na área digital."
+            />
+          </Head>
           <body>
+          <ColorModeScript initialColorMode={ColorTheme.config.initialColorMode} />
             <Main />
             <NextScript />
           </body>
