29e1f741f8752e0b83a85c57e63e86a5affe95a1
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-14 15:46:24 +0100

    create and configure footer component

---
+++ src/components/Footer/SocialMediaLinks.tsx
@@ -0,0 +1,11 @@
+import { ButtonGroup, ButtonGroupProps, IconButton } from '@chakra-ui/react'
+import * as React from 'react'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+export const SocialMediaLinks = (props: ButtonGroupProps) => (
+  <ButtonGroup variant="ghost" color="gray.600" {...props}>
+    <IconButton as="a" href="https://instagram.com/3Exitus-1002793" aria-label="Instagram" target="_blank" icon={<FaInstagram fontSize="30px" />} />
+    <IconButton as="a" href="#" aria-label="Facebook" target="_blank" icon={<FaFacebook fontSize="30px" />} />
+    <IconButton as="a" href="#" aria-label="Linkedin" target="_blank" icon={<FaLinkedin fontSize="30px" />} />
+  </ButtonGroup>
+)
