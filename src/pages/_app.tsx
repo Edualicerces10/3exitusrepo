2fdafa977904d1ed113846be7e48ed6c44b2c19a
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-15 14:33:56 +0100

    add new meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,8 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap" rel="stylesheet"/>
 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="%PUBLIC_URL%/3exitus.png" />
       </Head>
       <NabarApp />
       <Component {...pageProps} />
