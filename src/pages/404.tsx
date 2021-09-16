052760742fa998d0bb10a9e054d4d8bc4d061f13
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-16 18:48:49 +0100

    customize page 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,17 @@ import {Box, Container } from "@chakra-ui/react";
 
 import { Image } from '@chakra-ui/react'
 import React from "react";
+import TextError404 from "../components/ErrorCustom/text";
 export default function Custom404(){
     return (
         <Container>
+        <TextError404 />
         <Box
+            as='div'
             display="flex"
-            alignItems="center"
-            justifyContent="center"
+            // alignItems="center"
+            // justifyContent="center"
+            px={{ base: '4', md: '8' }}
         >
             <Image src="/img/Oops404.svg" alt="Segun Adebayo" />
         </Box>
