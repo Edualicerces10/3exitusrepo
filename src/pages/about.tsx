81018af7c9749bd58ad50165e9e749fd58dd72f8
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-16 18:52:47 +0100

    create about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+
+
+export default function About() {
+    return(
+        <Box as='div' minHeight="70vh" px={{ base: '4', md: '8' }}>
+            <span>Exemplo</span>
+        </Box>
+    )
+}
