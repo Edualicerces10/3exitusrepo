931d3c1fe211a31d8477dcae2f9be7e735937edf
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-17 16:25:27 +0100

    Add section service section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import { Box, Divider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import React from "react";
-import DividerSections from "../components/DividerSections";
 import HeroSection  from "../components/Hero";
+import SectionServices from "../components/SectionServices";
 
 
 export default function Home(){
   return (
-      <Box as='div' minHeight="70vh" px={{ base: '4', md: '8' }}>
+      <>
         <HeroSection />
-      </Box>
+        <SectionServices />
+      </>
   )
 }
