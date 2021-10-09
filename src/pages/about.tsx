3e71cc9631dbeeb2239ff56f3ed84e83f1c4ae06
Edualicerces10 <eduardoalicerces@hotmail.com>
2021-10-09 10:13:40 +0100

    add title in page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,16 @@
 import { Box } from '@chakra-ui/react'
+import Head from 'next/head'
 
 
 export default function About() {
     return(
+    <>
+        <Head>
+            <title>Sobre Nós | exitus</title>
+        </Head>
         <Box as='div' minHeight="70vh" px={{ base: '4', md: '8' }}>
             <span>Exemplo</span>
-        </Box>
+        </Box> 
+    </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import HeroSection  from "../components/Hero";
+import SectionFeactures from "../components/SectionFeatures";
 // import SectionMarketing from "../components/SectionMarketing";
 import SectionServices from "../components/SectionServices";
+
+import Head from 'next/head'
 
 
 export default function Home(){
   return (
       <>
+        <Head>
+            <title>Home | exitus</title>
+        </Head>
         <HeroSection />
         <SectionServices />
         {/* <SectionMarketing /> */}
+        <SectionFeactures />
       </>
   )
 }
