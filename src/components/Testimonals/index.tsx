dc8ae79793dd0068e0a6e341641d838c07ed37f5
Edualicerces10 <eduardoalicerces@hotmail.com>
2021-10-14 21:33:49 +0100

    add section testimonals

--- src/components/Testimonals/index.tsx
+++ src/components/Testimonals/index.tsx
@@ -2,7 +2,6 @@ import {
     Avatar,
     Box,
     chakra,
-    Container,
     Flex,
     Icon,
     SimpleGrid,
@@ -10,14 +9,6 @@ import {
   } from '@chakra-ui/react';
   
   const testimonials = [
-    {
-      name: 'Brandon P.',
-      role: 'Chief Marketing Officer',
-      content:
-        'It really saves me time and effort. It is exactly what our business has been lacking. EEZY is the most valuable business resource we have EVER purchased. After using EEZY my business skyrocketed!',
-      avatar:
-        'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
-    },
     {
       name: 'Krysta B.',
       role: 'Entrepreneur',
@@ -130,14 +121,15 @@ function TestmonialCard(props: TestimonialCardProps) {
     );
   }
   
-  export default function GridBlurredBackdrop() {
+  export default function TestimonalSection() {
     return (
       <Flex
         textAlign={'center'}
         pt={10}
         justifyContent={'center'}
         direction={'column'}
-        width={'full'}>
+        width={'full'}
+        >
         <Box width={{ base: 'full', sm: 'lg', lg: 'xl' }} margin={'auto'}>
           <chakra.h3
             fontFamily={'Work Sans'}
@@ -169,7 +161,7 @@ function TestmonialCard(props: TestimonialCardProps) {
           </chakra.h2>
         </Box>
         <SimpleGrid
-          columns={{ base: 1, xl: 2 }}
+          columns={{ base: 1, xl: 1 }}
           spacing={'20'}
           mt={16}
           mx={'auto'}>
