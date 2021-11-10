1554666c893a73bf78029eb0d99ccaa054db1829
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-11-10 08:37:09 +0000

    edit texts

--- src/components/OtherFeatures/index.tsx
+++ src/components/OtherFeatures/index.tsx
@@ -1,76 +1,60 @@
-import React from "react";
+import React from "react"
 
-import {
-  Box,
-  chakra,
-  SimpleGrid,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import DividerSections from "../DividerSections";
-import TabNewMarketing from "../TabNewMarketing";
-import { Image } from './image'
+import { Box, chakra, SimpleGrid, useColorModeValue } from "@chakra-ui/react"
+import DividerSections from "../DividerSections"
+import TabNewMarketing from "../TabNewMarketing"
+import { Image } from "./image"
 
 export default function OtherFeatures() {
-  return (
-    <>
-    <Box
-        mx="auto" 
-        pt="4" 
-        px={{ base: '4', md: '8' }}
-        justifyContent="center"
-        alignItems="center"
-    >
-        <SimpleGrid
-          alignItems="start"
-          columns={{ base: 1, md: 2 }}
-          spacingY={{ base: 10, md: 32 }}
-          spacingX={{ base: 10, md: 32 }}
-        >
-          <Box>
-            <chakra.h2
-              mb={4}
-              fontSize={{ base: "xl", md: "3xl" }}
-              letterSpacing="tight"
-              textAlign={{ base: "center", md: "left" }}
-              color={useColorModeValue("purple.600", "gray.400")}
-              lineHeight={{ md: "shorter" }}
-              textShadow="2px 0 currentcolor"
+    return (
+        <>
+            <Box
+                mx="auto"
+                pt="4"
+                px={{ base: "4", md: "8" }}
+                justifyContent="center"
+                alignItems="center"
             >
-              Conheça as áreas do marketing que actuamos
-            </chakra.h2>
-            <chakra.p
-              mb={10}
-              textAlign={{ base: "center", sm: "left" }}
-            //   color={useColorModeValue("gray.600", "gray.400")}
-              fontSize={{ md: "lg" }}
-              lineHeight={8}
-            >
-<<<<<<< HEAD
-            Para melhor compressão do que fazemos, aprensentamos aqui em que podemos melhoras ou mudar o seu negócio.
-=======
-            Para melhor compressão do que fazemos, aprensentamos aqui em que podemos melhoras ou mudar o seu negócio. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus necessitatibus architecto consectetur.
->>>>>>> 69c9ea667587b1ff310a00f61da750dbd3d3cb04
-            </chakra.p>
+                <SimpleGrid
+                    alignItems="start"
+                    columns={{ base: 1, md: 2 }}
+                    spacingY={{ base: 10, md: 32 }}
+                    spacingX={{ base: 10, md: 32 }}
+                >
+                    <Box>
+                        <chakra.h2
+                            mb={4}
+                            fontSize={{ base: "xl", md: "3xl" }}
+                            letterSpacing="tight"
+                            textAlign={{ base: "center", md: "left" }}
+                            color={useColorModeValue("purple.600", "gray.400")}
+                            lineHeight={{ md: "shorter" }}
+                            textShadow="2px 0 currentcolor"
+                        >
+                            Conheça as áreas do marketing que actuamos
+                        </chakra.h2>
+                        <chakra.p
+                            mb={10}
+                            textAlign={{ base: "center", sm: "left" }}
+                            //   color={useColorModeValue("gray.600", "gray.400")}
+                            fontSize={{ md: "lg" }}
+                            lineHeight={8}
+                        >
+                            Para melhor compressão do que fazemos, aprensentamos
+                            aqui em que podemos melhoras ou mudar o seu negócio.
+                        </chakra.p>
 
-            <Image
-                src='/img/businessexitus.svg'
-                alt="afasdas"
-              />
-          </Box>
-          <Box
-            // py={40}
-            //bg={useColorModeValue("gray.200", "gray.700")}
-          >
-              {/* <Image
-                src='/img/designexitus.svg'
-                alt="afasdas"
-              /> */}
-              <TabNewMarketing />
-          </Box>
-        </SimpleGrid>
-      </Box>
-    {/* <TabNewMarketing /> */}
-    <DividerSections/>
-    </>
-  )
+                        <Image src="/img/businessexitus.svg" alt="afasdas" />
+                    </Box>
+                    <Box
+                    // py={40}
+                    //bg={useColorModeValue("gray.200", "gray.700")}
+                    >
+                        <TabNewMarketing />
+                    </Box>
+                </SimpleGrid>
+            </Box>
+            <DividerSections />
+        </>
+    )
 }
