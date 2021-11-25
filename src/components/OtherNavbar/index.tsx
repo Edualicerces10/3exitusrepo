225fd6dac332a28f569276e0c9a094ec2ee82a19
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-11-25 11:07:00 +0000

    add new component topcontacts

--- src/components/OtherNavbar/index.tsx
+++ src/components/OtherNavbar/index.tsx
@@ -5,10 +5,12 @@ import {
 } from '@chakra-ui/react'
 import { ColorModeSwitcher } from '../ButtonColorModeSwitch'
 import Logo from '../Logo'
+import TopContacts from '../TopContacts'
 
 export default function OtherNavbar() {
   return (
     <>
+      <TopContacts />
       <Box as="header" role="contentheader" mx="auto" px={8}>
         <Flex h={14} alignItems={'center'} justifyContent={'space-between'}>
           <Box>
